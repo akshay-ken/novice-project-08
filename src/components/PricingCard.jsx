dea5773d44a48cb1d64eb2c9e0c41e3677ba03f6
akshay-ken <akshaybest3120@gmail.com>
2025-08-13 21:45:59 +0530

    feat: usestate hook to set value prop and select curretn pricing

--- src/components/PricingCard.jsx
+++ src/components/PricingCard.jsx
@@ -1,6 +1,26 @@
+import { useState } from "react";
+
+const pricingDetails = [
+  { pageviews: "10K", price: "8" },
+  { pageviews: "50K", price: "12" },
+  { pageviews: "100K", price: "16" },
+  { pageviews: "500k", price: "24" },
+  { pageviews: "1M", price: "36" },
+];
+
 export function PricingCard() {
+  const [sliderValue, setSliderValue] = useState(1);
+  let currentPriceIndex = 0;
+  const currentData = pricingDetails[currentPriceIndex];
+  const [curretnPrice, setCurrentPrice] = useState(pricingDetails[0].price);
+
+  function handleValueChange(event) {
+    setSliderValue(event.target.value);
+    currentPriceIndex = Math.ceil(sliderValue / 20) - 1;
+    setCurrentPrice(pricingDetails[currentPriceIndex].price);
+  }
   return (
-    <section className="w-full md:w-[50%] bg-white flex flex-col items-center justify-center mt-8">
+    <section className="w-full md:w-[50%] bg-white flex flex-col items-center justify-center mt-8 p-4">
       <p className=" uppercase text-base font-bold text-Text ">
         100k Pageviews
       </p>
@@ -9,25 +29,22 @@ export function PricingCard() {
       <input
         id="steps-range"
         type="range"
-        min={0}
+        onChange={handleValueChange}
+        min={1}
         max={100}
-        value={50}
-        step={2}
+        value={sliderValue}
+        step={1}
         style={{
-          backgroundImage: `linear-gradient(90deg, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) ${50}%, gray ${50}%, gray 100%)`,
+          backgroundImage: `linear-gradient(90deg, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) ${sliderValue}%, hsl(224, 65%, 95%) ${sliderValue}%, hsl(224, 65%, 95%) 100%)`,
         }}
         className={"w-full my-12 h-3 rounded-lg appearance-none cursor-pointer"}
       />
+      <div className="flex flex-row justify-center gap-x-4 items-center">
+        <p className="text-5xl font-extrabold text-CTA-Background">
+          ${curretnPrice}.00{" "}
+        </p>
+        <p className="text-xl font-semibold text-Text mt-1">/ month</p>
+      </div>
     </section>
   );
 }
-
-// Here are the different page view ranges and the corresponding monthly price totals:
-
-// - 10K pageviews / $8 per month
-// - 50K pageviews / $12 per month
-// - 100K pageviews / $16 per month
-// - 500k pageviews / $24 per month
-// - 1M pageviews / $36 per month
-
-// If the visitor switches the toggle to yearly billing, a 25% discount should be applied to all prices.
